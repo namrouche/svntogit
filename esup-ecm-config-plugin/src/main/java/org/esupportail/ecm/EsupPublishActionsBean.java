bf836ecb6b636a30b34bdb1ffed87ab53d644525
vbonamy <vbonamy@2bebc3db-464a-0410-8034-b528f6c09253>
2008-09-30 13:37:39 +0000

    esup-ecm-config: in progress .....
    git-svn-id: http://subversion.cru.fr/esup-ecm/esup-ecm-config/trunk@113 2bebc3db-464a-0410-8034-b528f6c09253

--- esup-ecm-config-plugin/src/main/java/org/esupportail/ecm/EsupPublishActionsBean.java
+++ esup-ecm-config-plugin/src/main/java/org/esupportail/ecm/EsupPublishActionsBean.java
@@ -5,22 +5,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.faces.application.FacesMessage;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.RequestParameter;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Transactional;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentModelTree;
+import org.nuxeo.ecm.core.api.DocumentModelTreeNode;
 import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.VersionModel;
 import org.nuxeo.ecm.core.api.facet.VersioningDocument;
-import org.nuxeo.ecm.core.api.impl.DocumentModelTreeImpl;
-import org.nuxeo.ecm.core.api.impl.DocumentModelTreeNodeImpl;
 import org.nuxeo.ecm.platform.publishing.PublishActionsBean;
 import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 import org.nuxeo.ecm.platform.versioning.api.VersioningManager;
@@ -48,6 +50,10 @@ public class EsupPublishActionsBean extends PublishActionsBean {
     
     @In(create = true)
     protected transient VersioningManager versioningManager;
+    
+    @RequestParameter
+    private String versionSelectedRef;
+    
 
     public List getVersionsSelectModel() throws ClientException {
 		
@@ -64,8 +70,12 @@ public class EsupPublishActionsBean extends PublishActionsBean {
                 VersioningDocument docVer = tempDoc.getAdapter(VersioningDocument.class);
 
                 String versionLabel = versioningManager.getVersionLabel(tempDoc);
-                if(!versionsDoc.containsKey(versionLabel))
-                	versionsDoc.put(versionLabel, new DocumentModelTreeImpl());
+                if(!versionsDoc.containsKey(versionLabel)) {
+                	List values = new ArrayList();
+                	values.add(tempDoc);
+                	values.add(new ArrayList());
+                	versionsDoc.put(versionLabel, values);
+                }
                 
                 for(DocumentModel proxy : documentManager.getProxies(tempDoc.getRef(), null)) {
                 	DocumentRef parentRef = proxy.getParentRef();
@@ -75,7 +85,7 @@ public class EsupPublishActionsBean extends PublishActionsBean {
                     l.add(proxy);
                     l.add(section);
                     l.add(dublincoreProperties);
-                    versionsDoc.get(versionLabel).add(l);
+                    ((List)versionsDoc.get(versionLabel).get(1)).add(l);
                 }
             }
             
@@ -92,5 +102,92 @@ public class EsupPublishActionsBean extends PublishActionsBean {
        
        return versionsDocList;
 	}
+    
+    
+    
+    /*
+     * Called by action ESUP_DOCUMENT_PUBLISH.
+     */
+    public String publishVersion() throws ClientException {
+        DocumentModel docToPublish = documentManager.getDocument(new IdRef(versionSelectedRef));
+
+        
+        if (documentManager.getLock(docToPublish.getRef()) != null) {
+            facesMessages.add(FacesMessage.SEVERITY_WARN,
+                    resourcesAccessor.getMessages().get(
+                            "error.document.locked.for.publish"));
+            return null;
+        }
+
+        List<DocumentModelTreeNode> selectedSections = getSelectedSections();
+        if (selectedSections.isEmpty()) {
+            facesMessages.add(FacesMessage.SEVERITY_WARN,
+                    resourcesAccessor.getMessages().get(
+                            "publish.no_section_selected"));
+
+            return null;
+        }
+
+        log.debug("selected " + selectedSections.size() + " sections");
+
+        /**
+         * Proxies for which we need a moderation. Let's request the moderation
+         * after the document manager session has been saved to avoid conflicts
+         * in between sync txn and async txn that can start before the end of
+         * the sync txn.
+         */
+        List<DocumentModel> forModeration = new ArrayList<DocumentModel>();
+
+        for (DocumentModelTreeNode section : selectedSections) {
+            boolean moderation = !isAlreadyPublishedInSection(docToPublish,
+                    section.getDocument());
+
+            
+            VersionModel versionModel =  documentManager.getVersionsForDocument(docToPublish.getRef()).get(0);
+            DocumentModel proxy = documentManager.createProxy(section.getDocument().getRef(), docToPublish.getRef(), versionModel, true);
+
+            if (moderation && !isReviewer(proxy)) {
+                forModeration.add(proxy);
+            }
+        }
+
+        // A document is considered published if it doesn't have
+        // approval from section's manager
+        boolean published = false;
+        if (selectedSections.size() > forModeration.size()) {
+            published = true;
+        }
+
+        if (published) {
+
+            // notifyEvent(org.nuxeo.ecm.webapp.helpers.EventNames.DOCUMENT_PUBLISHED,
+            // null, comment,
+            // null, docToPublish);
+
+            facesMessages.add(FacesMessage.SEVERITY_INFO,
+                    resourcesAccessor.getMessages().get("document_published"),
+                    resourcesAccessor.getMessages().get(docToPublish.getType()));
+        }
+
+        if (!forModeration.isEmpty()) {
+            // notifyEvent(org.nuxeo.ecm.webapp.helpers.EventNames.DOCUMENT_SUBMITED_FOR_PUBLICATION,
+            // null, comment,
+            // null, docToPublish);
+            facesMessages.add(FacesMessage.SEVERITY_INFO,
+                    resourcesAccessor.getMessages().get(
+                            "document_submitted_for_publication"),
+                    resourcesAccessor.getMessages().get(docToPublish.getType()));
+            for (DocumentModel proxy : forModeration) {
+                notifyEvent(
+                        org.nuxeo.ecm.webapp.helpers.EventNames.PROXY_PUSLISHING_PENDING,
+                        null, null, null, proxy);
+            }
+        }
+
+        setSectionsModel(null);
+
+        return null;
+    }
+
     
 }
